c587c4a2ff0376002e800728fc911eac24befeb3
Andriy Shapochka <ashapochka@gmail.com>
2019-04-02 20:05:02 +0300

    medium data prep - fewer files, more data

--- genmed.sh
+++ genmed.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 mkdir -p /mnt/efs/pipetest2
-./gendata.py /mnt/efs/pipetest2/medpipe -f 10 -c 10 -r 10000000 -z -t medpipe
+./gendata.py /mnt/efs/pipetest2/medpipe -f 4 -c 10 -r 100000000 -z -t medpipe
